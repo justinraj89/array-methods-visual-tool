b6d6b8051a7c2ba7a9198b58db3934a4b0f78942
Justin Rajewski <rajewskijustin@gmail.com>
2023-05-01 12:34:40 -0700

    setAnimatedIndex state setup so that there is a visual on the element being removed/added

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ function App() {
 
   const [arr, setArr] = useState(testArray);
   const [itemToPush, setItemToPush] = useState('');
+  const [animatedIndex, setAnimatedIndex] = useState(null)
 
 
   function handlePushItem(e) {
     e.preventDefault();
     if(!itemToPush) return
+    setAnimatedIndex(arr.length);
+    setTimeout(() => {
+      setAnimatedIndex(null)
+    }, 1000)
     setArr([...arr, itemToPush]);
     setItemToPush('')
   }
   
   function handlePop() {
-    let newArr = [...arr];
-    newArr.pop();
-    setArr(newArr)
+    setAnimatedIndex(arr.length - 1);
+    setTimeout(() => {
+      let newArr = [...arr];
+      newArr.pop();
+      setArr(newArr);
+      setAnimatedIndex(-1);
+    }, 1000);
   }
 
   return (
@@ -28,7 +37,18 @@ function App() {
       <h2 className="font-mono font-bold text-lg">a visual representation</h2>
 
       <div className="mt-4 flex flex-col w-5/6 lg:w-1/3 justify-center items-center bg-gray-300 rounded-lg py-10">
-        <p className="text-4xl">[{arr.join(', ')}]</p>
+
+      <p className="text-4xl">[{" "}
+          {arr.map((item, index) => (
+            <span
+              key={index}
+              className={`inline-block ${index === animatedIndex ? "bg-red-400" : ""}`}
+            >
+              {item}{index !== arr.length - 1 && ", "}
+            </span>
+          ))}
+          {" ]"}
+        </p>
 
         <section className="px-4 mt-4 rounded-lg bg-gray-600 py-2 flex items-center justify-center">
           <form onSubmit={handlePushItem} className="flex gap-4 font-mono">
@@ -39,11 +59,15 @@ function App() {
               <option value='😇'>😇</option>
               <option value='😢'>😢</option>
               <option value='👨🏽‍💻'>👨🏽‍💻</option>
+              <option value='🙈'>🙈</option>
+              <option value='❤️'>❤️</option>
             </select>
-            <button type="submit" className="px-2 bg-slate-500 rounded-lg text-white">Push</button>
-            <button onClick={handlePop} className="px-2 bg-slate-500 rounded-lg text-white">Pop</button>
+            <button type="submit" className="px-2 bg-slate-500 hover:bg-slate-400 rounded-lg text-white">Push</button>
+            <button onClick={handlePop} className="px-2 bg-slate-500 hover:bg-slate-400 rounded-lg text-white">Pop</button>
         </form>
         </section>
+
+
        
 
       </div>
@@ -53,3 +77,6 @@ function App() {
 }
 
 export default App;
+
+
+{/* <p className="text-4xl">[{arr.join(', ')}]</p> */}
